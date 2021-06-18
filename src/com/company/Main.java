7a76341aa8292a1144e1e2fda6ed55d634cf9a6b
ZannaJ <zhanna.jelatanceva@gmail.com>
2021-06-18 20:41:09 +0300

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Ints
+        int firstNumber;
+        int secondNumber;
+        firstNumber = 5;
+        secondNumber = 6;
+
+        int sum = firstNumber + secondNumber;
+  //      System.out.println(sum);
+
+        // Floats
+        float firstFloat = 0.5f;
+        float secondFloat = 2.3f;
+
+        float floatSum = firstFloat + secondFloat;
+//      System.out.println(floatSum/4);
+
+        // Doubles
+        double firstDouble = 0.7;
+        double secondDouble = 2.4;
+        double doubleSum = firstDouble +secondDouble;
+
+ //       System.out.println(doubleSum);
+
+        String firstString = "Chips";
+        String secondString = "Chicken";
+
+        System.out.println("I love " + firstString + " and " + secondString);
+
+        
+
+
+
+    }
+}
